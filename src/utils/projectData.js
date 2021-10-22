523503f3418bfd205f5dd06b9c257a267a47cccf
Kyle <you@example.com>
2021-10-22 09:45:17 -0500

    updates

--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -2,7 +2,8 @@ import ni from "../assets/projects/ni.png";
 import nwbs from "../assets/projects/nwbs.png";
 import rrr from "../assets/projects/rrr.png"; 
 import pg from '../assets/projects/pg-min 2.png'
-import rs from '../assets/projects/rs-min.png'
+import rs from '../assets/projects/rs-min.png';
+import sbw from '../assets/projects/sbWebsites-min.png'
 
 export const projectData = [
   {
@@ -35,6 +36,14 @@ export const projectData = [
     description:
       "I built a fully responsive website wising the latest React framework",
     img: rs,
+    id: "3",
+  },
+  {
+    title: "Nationwide Investigations",
+    website: "https://www.nwbs-inc.com/",
+    description:
+      "I built a fully responsive website wising the latest React framework",
+    img: sbw,
     id: "3",
   },
 ];
